925e955f1dc36731ef29bdd930c3810bcf7f1998
bistenofficial <maks.kirov.2002@mail.ru>
2021-03-10 12:01:46 +0300

    Общ часть

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView helloTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloTv = (TextView) findViewById(R.id.hello_tv);
+        TextView helloTv = (TextView) findViewById(R.id.hello_tv);
     }
-}
+    public void onclicked(View v) {
+        switch (v.getId()) {
+
+            case R.id.hello_btn:
+                helloTv.setText("Обработка нажатия на кнопку ");
+                break;
+
+            case R.id.hello_tv:
+                helloTv.setText("Нажали на TextView ");
+                break;
+
+        }
+
+}}
